a11b64fe8555426eb4896e4f337cec763f2807e0
giorgos <giorgos@ccs.neu.edu>
2016-11-30 08:37:58 -0500

    fix the stats script

--- stats.sh
+++ stats.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-OUTPUT="$(awk '{print $1}' "$1"| uniq -c | grep read | wc -l)"
+OUTPUT="$(grep read "$1" | awk '{sum = sum+$3}END{print sum}')"
 echo "Blocks read   :  ${OUTPUT}"
 
-OUTPUT="$(awk '{print $1}' "$1"| uniq -c | grep write | wc -l)"
+OUTPUT="$(grep write "$1" | awk '{sum = sum+$3}END{print sum}')"
 echo "Blocks written:  ${OUTPUT}"
 
-OUTPUT="$(grep read "$1" | awk '{sum = sum+$3}END{print sum}')"
+OUTPUT="$(awk '{print $1}' "$1"| uniq -c | grep read | wc -l)"
 echo "Read ops      :  ${OUTPUT}"
 
-OUTPUT1="$(grep write "$1" | awk '{sum = sum+$3}END{print sum}')"
+OUTPUT1="$(awk '{print $1}' "$1"| uniq -c | grep write | wc -l)"
 echo "Write ops     :  ${OUTPUT1}"
 echo "---------------"
 echo "Total ops     :  $((OUTPUT + OUTPUT1))"
